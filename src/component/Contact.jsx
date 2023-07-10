9bc95bf22454368614270bcddaf75efa5384d4ba
Roshan <roshanpawar119@gmail.com>
2023-07-10 15:18:54 +0530

    changes in Contact.jsx file

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -21,8 +21,8 @@ function Contact() {
     })
   }, [])
 
-  function clickOnBtn(){
-      
+  function formSubmit(e){
+    e.preventDefault();  
     alert("Demo form");
     setFormdata({name: "",email: "", message: ""})
   }
@@ -47,7 +47,7 @@ function Contact() {
           <p>Contact me</p>
         </div>
         <div className="contact-me-form" >
-          <form >
+          <form onSubmit={formSubmit}>
             <div className="form-group">
               <label htmlFor="name">Name:</label>
               <input type="text" id="name" name="name" value={formdata.name} onChange={handleForm}required />
@@ -61,7 +61,7 @@ function Contact() {
               <textarea id="message" name="message" value={formdata.message} onChange={handleForm}rows="4" placeholder='Write your project ' required></textarea>
             </div>
             <div className="form-group">
-              <button onClick={clickOnBtn}>Submit</button>
+              <button type='submit'>Submit</button>
             </div>
           </form>
         </div>
